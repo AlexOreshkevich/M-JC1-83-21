bf477cef7008dba4c8ee642bbad902158ad733ce
Qdesnitsa <88439744+Qdesnitsa@users.noreply.github.com>
2021-08-06 21:37:20 +0300

    Update MyClass.java
    1. File is put into the package (at least I hope so:))
    2. Formatting on lines 3 and 4 is corrected

--- MyClass.java
+++ MyClass.java
@@ -1,8 +1,9 @@
+package by.academy.jc.sidina;
 public class MyClass {
-  public static void main(String[] args) {
-String a = ("Don't Worry Be Happy");
-int[] smth_new = new int[] {404,100500,2021};
-    System.out.println(a);
-    System.out.println(smth_new[2]);
-  }
+    public static void main(String[] args) {
+        String a = ("Don't Worry Be Happy");
+        int[] smth_new = new int[] {404,100500,2021};
+        System.out.println(a);
+        System.out.println(smth_new[2]);
+    }
 }
