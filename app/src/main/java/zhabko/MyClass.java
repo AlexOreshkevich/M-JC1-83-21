82499e09e08f1a16ebd01477403214cdee6086be
Alexey_Zhabko <zhabkoag@gmail.com>
2021-08-06 18:35:18 +0300

    First homework

---
+++ app/src/main/java/zhabko/MyClass.java
@@ -0,0 +1,14 @@
+
+package zhabko;
+
+public class MyClass {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!"); // Basic example
+
+
+        String message = "Second example"; // Defining new variable "message"
+        System.out.println(message); // Sending message to the terminal
+
+        System.out.println(args[0]); // To see the result you have to use "javac command" and terminal
+    }
+}
