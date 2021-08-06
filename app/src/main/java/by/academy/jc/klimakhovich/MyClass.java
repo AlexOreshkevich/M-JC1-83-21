85fe31d2abca4fadff655c55330166b900a99737
Uladzislau <Klimaxovich@mail.ru>
2021-08-06 14:05:27 +0300

    add function

--- app/src/main/java/by/academy/jc/klimakhovich/MyClass.java
+++ app/src/main/java/by/academy/jc/klimakhovich/MyClass.java
@@ -1,8 +1,13 @@
 package by.academy.jc.klimakhovich;
 
+import by.academy.jc.App;
+
 public class MyClass {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(new MyClass().get_Greeting());
+    }
+    public String get_Greeting() {
+        return "Hello World!";
     }
 }
