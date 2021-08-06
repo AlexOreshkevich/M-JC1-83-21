11b6acaff3dc7e112e9416e7c17f515d22e170b1
Natalia-Dubautsova <natagromova.99@mail.ru>
2021-08-06 17:13:53 +0300

    first homework

---
+++ app/src/main/java/dubautsova/NewClass.java
@@ -0,0 +1,12 @@
+package dubautsova;
+
+public class NewClass {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        String message = "something";
+        System.out.println(message);
+        for (int i=0; i<args.length; i++) {
+            System.out.println(args[i]);
+        }
+    }
+}
