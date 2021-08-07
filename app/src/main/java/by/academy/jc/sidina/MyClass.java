d791b5a0fabae6f8105084f62574e7873ce16436
Qdesnitsa <lenkasid@tut.by>
2021-08-07 19:00:46 +0300

    first task

--- app/src/main/java/by/academy/jc/sidina/MyClass.java
+++ app/src/main/java/by/academy/jc/sidina/MyClass.java
@@ -1,5 +1,7 @@
 package by.academy.jc.sidina;
+
 public class MyClass {
+
     public static void main(String[] args) {
         String a = ("Don't Worry Be Happy");
         int[] smth_new = new int[] {404,100500,2021};
