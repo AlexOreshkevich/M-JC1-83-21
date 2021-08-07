cf88f7badc31616404f788bc5cdea771c5ae2713
Alena Grinchik <lena,grinchik@gmail.com>
2021-08-07 18:07:30 +0300

    hometask #1

---
+++ app/src/main/java/by/academy/jc/grinchik/MyClass.java
@@ -0,0 +1,14 @@
+package by.academy.jc.grinchik;
+
+public class MyClass {
+    public static void main(String[] args) {
+        String message = "something";
+        String version = "1.2.3.4";
+        System.out.println(message);
+        if (args.length > 0) {
+            if (args[0].equals("--version")){
+                System.out.println(version);
+            }
+        }
+    }
+}
