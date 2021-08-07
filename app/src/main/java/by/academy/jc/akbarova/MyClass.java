18ee568604b33b2867a57f4b893173483c5efaa1
Leyli2020 <74834945+Leyli2020@users.noreply.github.com>
2021-08-07 20:01:54 +0300

    Create MyClass.java
    Added new Akbarova file

---
+++ app/src/main/java/by/academy/jc/akbarova/MyClass.java
@@ -0,0 +1,10 @@
+package by.academy.jc.akbarova;
+
+public class MyClass {
+    public static void main (String[] args){
+        String message="Smile and be happy ;)";
+        System.out.println("Hello, world!");
+        System.out.println(message);
+
+    }
+}
