3a48201a085f38223fb8b167a6d6c7ac31ed7c41
Uladzislau <Klimaxovich@mail.ru>
2021-08-07 12:22:43 +0300

    fix 1

--- app/src/main/java/by/academy/jc/klimakhovich/MyClass.java
+++ app/src/main/java/by/academy/jc/klimakhovich/MyClass.java
@@ -3,11 +3,13 @@ package by.academy.jc.klimakhovich;
 
 public class MyClass {
 
+    public String greeting() {
+        return "Hello Vlad you are amazing!";
+    }
+
     public static void main(String[] args) {
         System.out.println(new MyClass().greeting());
     }
 
-    public String greeting() {
-        return "Hello Vlad you are amazing!";
-    }
+
 }
