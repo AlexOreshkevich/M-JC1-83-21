1b379deea2e55cfcb295fe09fde1bd30052a2db8
Artur Zabrodski <artur.zabrodski@gmail.com>
2021-08-08 17:20:45 +0300

    release version 1

--- app/src/main/java/by/academy/jc/Zabrodski/MyClass.java
+++ app/src/main/java/by/academy/jc/Zabrodski/MyClass.java
@@ -4,11 +4,9 @@ public class MyClass {
     public static void main(String[] args) {
       System.out.println("Hello, world!");  // message output "Hello, world!"
 
-      String message = "something";         // assign a value to the variable "something"
+      final String message = "something";         // assign a value to the variable "something"
 
-      //final String TEXT = message;          // initializing a constant variable and
-
-      //System.out.println(TEXT);             // message output "something"
+      System.out.println(message);             // message output "something"
 
       String[] arg = {"Привет", "Java"};    // array data
 
