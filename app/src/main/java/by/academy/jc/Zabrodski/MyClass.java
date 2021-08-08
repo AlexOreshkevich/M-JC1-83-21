583caa197ba001b954ce26a0d2f91c207aeffe56
Artur Zabrodski <artur.zabrodski@gmail.com>
2021-08-08 17:21:23 +0300

    release version 1

--- app/src/main/java/by/academy/jc/Zabrodski/MyClass.java
+++ app/src/main/java/by/academy/jc/Zabrodski/MyClass.java
@@ -2,14 +2,16 @@ package by.academy.jc.Zabrodski;
 
 public class MyClass {
     public static void main(String[] args) {
-      System.out.println("Hello, world!");  // message output "Hello, world!"
+      System.out.println("Hello, world!");     // message output "Hello, world!"
 
-      final String message = "something";         // assign a value to the variable "something"
+      String message = "something";            // assign a value to the variable "something"
 
-      System.out.println(message);             // message output "something"
+      final String constOfMessage = message;
 
-      String[] arg = {"Привет", "Java"};    // array data
+      System.out.println(constOfMessage);      // output "something"
 
-      System.out.println(arg[0]);           // output the first element of the array
+      String[] arg = {"Привет", "Java"};       // array data
+
+      System.out.println(arg[0]);              // output the first element of the array
     }
 }
