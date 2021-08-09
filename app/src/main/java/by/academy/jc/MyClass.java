03b9712f45a56cc3e93f05d21fa0b162b0a710b9
Valeria <sergeichik26@gmail.com>
2021-08-09 16:31:40 +0300

    1st hometask

---
+++ app/src/main/java/by/academy/jc/MyClass.java
@@ -0,0 +1,11 @@
+package by.academy.jc;
+
+public class MyClass {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+        String message = "Hello, git!";
+        System.out.println(message);
+    }
+}
+
+
