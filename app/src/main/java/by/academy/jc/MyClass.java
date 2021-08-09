754e6cd8b9d6c4366bf08d2f44449363302d02de
KateZaikouskaya <kzaikouskaya@yandex.by>
2021-08-09 14:38:17 +0300

    Typing "something" instead of "Hello World!".

---
+++ app/src/main/java/by/academy/jc/MyClass.java
@@ -0,0 +1,11 @@
+package by.academy.jc;
+
+public class MyClass {
+
+    public static void main(String[] args){
+        System.out.println(message);
+
+    }
+    public static String message = "something";
+
+}
