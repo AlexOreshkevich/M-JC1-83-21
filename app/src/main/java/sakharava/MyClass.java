3f5e2311068c24eb9683386384c60ef05f0adb32
arellera <sakharvaleria@gmail.com>
2021-08-09 12:00:52 +0300

    homework#1

---
+++ app/src/main/java/sakharava/MyClass.java
@@ -0,0 +1,11 @@
+package sakharava;
+
+public class MyClass {
+    public static void main(String[] args) {
+        String [] names = new String[] {"Sakharava","Valeryia","Stanislavavna"};
+        // объявили и создали массив,выделили на него 3 строки
+        for (int i = 0; i < 3; i++) {
+            System.out.println(names[i]);
+        }
+    }
+}
