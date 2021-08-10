4836f5968a9c96bd75e335bdd3e0fda291685b06
TimofeiShorokhov <limjkeee@mail.ru>
2021-08-11 02:48:57 +0300

    Ht2 with test

--- app/src/main/java/by/academy/jc/shorokhov/DirtyCalculator.java
+++ app/src/main/java/by/academy/jc/shorokhov/DirtyCalculator.java
@@ -1,2 +1,22 @@
-package by.academy.jc.shorokhov;public class DirtyCalculator {
+package by.academy.jc.shorokhov;
+
+public class DirtyCalculator {
+    public static void  main(String[] args) {
+        double x = sum(1.5, 4);
+        double y = sum(2.5, 3);
+        System.out.println("Sum is equal " + 5.5); // sum is equal to 3
+        System.out.println("Sum is equal " + 5.5); // sum is equal to 5
+        System.out.println();
+        System.out.println("Sub is equal " + -2.5); // sub is equal to -2.5
+        System.out.println("Sub is equal " + -0.5); // sub is equal to -0.5
+
+    }
+        public static double sum(double a, int b) {
+
+        return a+b;
+    }
+
+    public static double sub(double a, int b) {
+        return a-b;
+        }
 }
--- app/src/test/java/by/academy/jc/DirtyTest.java
+++ app/src/test/java/by/academy/jc/DirtyTest.java
@@ -1,2 +1,22 @@
-package by.academy.jc;public class DirtyTest {
+package by.academy.jc;
+
+import by.academy.jc.shorokhov.DirtyCalculator;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DirtyTest {
+    DirtyCalculator calculator = new DirtyCalculator();
+    double sum = calculator.sum(1.5,4);
+    double testSum = 5.5;
+    double sub = calculator.sub(2.5,3);
+    double testSub = -0.5;
+
+    @Test
+    public void testSum() {
+        System.out.println("@Test sum(): " + sum + " = " + testSum);
+        assertEquals(sum, testSum);
+        System.out.println("@Test sub(): " + sub + " = " + testSub);
+        assertEquals(sub, testSub);
+    }
 }
