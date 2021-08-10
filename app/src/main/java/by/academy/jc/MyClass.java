d6e833342937f1d8d3f886d03b257f5250e8df5b
Dyskop <dimaskop18@gmail.com>
2021-08-10 04:58:34 +0300

    My changes description

---
+++ app/src/main/java/by/academy/jc/MyClass.java
@@ -0,0 +1,10 @@
+package by.academy.jc;
+
+public class MyClass {
+    public static void main(String[] args){
+        System.out.println("Hello, World!");
+        String message = "something";
+        System.out.println(message);
+        for (String arg : args) System.out.println(arg);
+    }
+}
