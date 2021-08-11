7abe16b7d9ea4a3f13e57f77b67719d9ac104124
Uladzislau <Klimaxovich@mail.ru>
2021-08-11 12:28:00 +0300

    2nd hometask

---
+++ app/src/main/java/by/academy/jc/klimakhovich_ht2/Calculator.java
@@ -0,0 +1,16 @@
+package by.academy.jc.klimakhovich_ht2;
+
+public class Calculator {
+    public double sum(int a, double b) {
+        return a + b;
+    }
+
+    public double multi(double a, int b) {
+        return a * b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Calculator().sum(5, 5.5));
+        System.out.println(new Calculator().multi(5.5, 6));
+    }
+}
