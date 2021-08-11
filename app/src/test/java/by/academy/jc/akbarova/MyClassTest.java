b0ab6c2e2f0fdba93c48645c6d0fa0c3b946790e
Leyli2020 <74834945+Leyli2020@users.noreply.github.com>
2021-08-11 12:52:51 +0300

    Update MyClassTest.java
    Deleted unusable assertNotNull

--- app/src/test/java/by/academy/jc/akbarova/MyClassTest.java
+++ app/src/test/java/by/academy/jc/akbarova/MyClassTest.java
@@ -4,8 +4,6 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-
 class MyClassTest {
 
     @Test
