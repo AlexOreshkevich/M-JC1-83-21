23e4b32c4464d6c8ec68999bcdeb9bf7b45e3a8a
Alexey_Zhabko <zhabkoag@gmail.com>
2021-08-12 21:30:57 +0300

    Increasing test coverage

--- app/src/test/java/by/academy/jc/zhabko/DirtySumTest.java
+++ app/src/test/java/by/academy/jc/zhabko/DirtySumTest.java
@@ -6,7 +6,7 @@ package by.academy.jc.zhabko;
     import static org.junit.jupiter.api.Assertions.assertEquals;
 public class DirtySumTest {
     @Test
-    void sumTest() {
+    void sumadndivTest() {
         DirtySum DirtySumObTest = new DirtySum();
         //Int unit test
         assertEquals(DirtySumObTest.sum(7,12),103);
@@ -15,11 +15,17 @@ public class DirtySumTest {
         //Double unit test
         assertEquals(DirtySumObTest.sum(2.5, 3.2), 13.7);
         //Short unit test
-        assertEquals(DirtySumObTest.sum(11, 23), 287);
+        short x1 = 11;
+        short x2 = 23;
+        assertEquals(DirtySumObTest.sum(x1,x2),287);
         //Long unit test
-        assertEquals(DirtySumObTest.sum(13,22), 321);
+        long x3 = 13;
+        long x4 = 22;
+        assertEquals(DirtySumObTest.sum(x3,x4), 321);
         //byte unit test
-        assertEquals(DirtySumObTest.sum(5,2),17);
+        byte x5 = 5;
+        byte x6 = 2;
+        assertEquals(DirtySumObTest.sum(x5,x6),17);
         //Division test
         assertEquals(DirtySumObTest.division(9.3,3.2),2.90625);
     }
