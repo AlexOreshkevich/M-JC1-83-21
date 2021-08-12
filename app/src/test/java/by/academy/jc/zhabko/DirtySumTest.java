c439aaf0f3691b0f2672456ea8f44f5901cf595e
Alexey_Zhabko <zhabkoag@gmail.com>
2021-08-13 02:48:33 +0300

    unit test remake

--- app/src/test/java/by/academy/jc/zhabko/DirtySumTest.java
+++ app/src/test/java/by/academy/jc/zhabko/DirtySumTest.java
@@ -9,25 +9,25 @@ public class DirtySumTest {
     void sumadndivTest() {
         DirtySum DirtySumObTest = new DirtySum();
         //Int unit test
-        assertEquals(DirtySumObTest.sum(7,12),103);
+        assertEquals(103,DirtySumObTest.sum(7,12), "must return 103");
         //Float unit test
-        assertEquals(DirtySumObTest.sum(2.5F, 3.2F),13.7F);
+        assertEquals(13.7F,DirtySumObTest.sum(2.5F, 3.2F), "must return 13.7F");
         //Double unit test
-        assertEquals(DirtySumObTest.sum(2.5, 3.2), 13.7);
+        assertEquals(13.7, DirtySumObTest.sum(2.5, 3.2), "must return 13.7");
         //Short unit test
         short x1 = 11;
         short x2 = 23;
-        assertEquals(DirtySumObTest.sum(x1,x2),287);
+        assertEquals(287,DirtySumObTest.sum(x1,x2), "must return 287");
         //Long unit test
         long x3 = 13;
         long x4 = 22;
-        assertEquals(DirtySumObTest.sum(x3,x4), 321);
+        assertEquals(321,DirtySumObTest.sum(x3,x4), "must return 321");
         //byte unit test
         byte x5 = 5;
         byte x6 = 2;
-        assertEquals(DirtySumObTest.sum(x5,x6),17);
+        assertEquals(17,DirtySumObTest.sum(x5,x6), "must return 17");
         //Division test
-        assertEquals(DirtySumObTest.division(9.3,3.2),2.90625);
+        assertEquals(2.90625,DirtySumObTest.division(9.3,3.2), "must return 2.90625");
     }
 
 }
