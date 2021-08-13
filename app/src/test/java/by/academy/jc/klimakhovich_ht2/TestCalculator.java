8bc3eba655a2127e76f2d21b2f0c085ed0479072
Uladzislau <Klimaxovich@mail.ru>
2021-08-13 09:40:42 +0300

    fix codacy

--- app/src/test/java/by/academy/jc/klimakhovich_ht2/TestCalculator.java
+++ app/src/test/java/by/academy/jc/klimakhovich_ht2/TestCalculator.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestCalculator {
     @Test
-    void DirtySum() {
+    void dirtySum() {
         Calculator classUnderTest = new Calculator();
         assertEquals(45.2, classUnderTest.sum1(5, 6.7), "sum1 should have 45.2");
         assertEquals(21.5, classUnderTest.sum2(3.5, 4), "sum2 should have 21.5");
