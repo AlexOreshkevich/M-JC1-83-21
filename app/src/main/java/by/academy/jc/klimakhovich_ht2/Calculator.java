48a65d0ec9b66bb3d6080a7bd500ff0584c1b838
Uladzislau <Klimaxovich@mail.ru>
2021-08-13 09:36:17 +0300

    fix 1

--- app/src/main/java/by/academy/jc/klimakhovich_ht2/Calculator.java
+++ app/src/main/java/by/academy/jc/klimakhovich_ht2/Calculator.java
@@ -1,16 +1,16 @@
 package by.academy.jc.klimakhovich_ht2;
 
 public class Calculator {
-    public double sum(int a, double b) {
-        return a + b;
+    public double sum1(int a, double b) {
+        return a + b + a * b;
     }
 
-    public double multi(double a, int b) {
-        return a * b;
+    public double sum2(double a, int b) {
+        return a + b + a * b;
     }
 
     public static void main(String[] args) {
-        System.out.println(new Calculator().sum(5, 5.5));
-        System.out.println(new Calculator().multi(5.5, 6));
+        System.out.println(new Calculator().sum1(5, 6.7));
+        System.out.println(new Calculator().sum2(3.5, 4));
     }
 }
