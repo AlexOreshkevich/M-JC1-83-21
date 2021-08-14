dc022315e423d28d889afe6f048b1088889a7fce
Artur Zabrodski <artur.zabrodski@gmail.com>
2021-08-15 02:02:29 +0300

    task 2. release v1.1

--- app/src/main/java/by/academy/jc/Zabrodski/ht2/DirtySumopen.java
+++ app/src/main/java/by/academy/jc/Zabrodski/ht2/DirtySumopen.java
@@ -2,23 +2,26 @@ package by.academy.jc.Zabrodski.ht2;
 
 public class DirtySumopen {
 
-  public static void main(String[] args) {
-
-    System.out.println("Результат = " + result(5, 3));
-    System.out.println("Результат = " + result(6L, 1L));
-    System.out.println("Результат = " + result(6.3, 1.3));
-  }
-
-  // adding the sum of numbers and the product of numbers
+  /**
+   * @return - returns the sum of the values of the sum of numbers and the multiplication of numbers
+   */
   public static int result(int a, int b) {
+
     return a + b + a * b;
   }
 
   public static long result(long a, long b) {
+
     return a + b + a * b;
   }
 
   public static double result(double a, double b){
+
+    return a + b + a * b;
+  }
+
+  public static float result(float a, float b){
+
     return a + b + a * b;
   }
 }
--- app/src/test/java/by/academy/jc/Zabrodski/ht2/DirtySupomenTest.java
+++ app/src/test/java/by/academy/jc/Zabrodski/ht2/DirtySupomenTest.java
@@ -5,12 +5,28 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 public class DirtySupomenTest {
-  @Test
-    public void shouldReturnDirtySum() {
 
-      DirtySumopen sum = new DirtySumopen();
-      assertEquals(23, sum.result(5, 3));
-      assertEquals(13L, sum.result(6L, 1L));
-      assertEquals(15.79, sum.result(6.3, 1.3));
-    }
+  DirtySumopen sum = new DirtySumopen();
+
+  @Test
+  public void shouldReturnDirtySumInt() {
+    assertEquals(23, sum.result(5, 3));
+  }
+
+  @Test
+  public void shouldReturnDirtySumLong() {
+    assertEquals(13L, sum.result(6L, 1L));
+  }
+
+  @Test
+  public void shouldReturnDirtySumDouble() {
+    assertEquals(15.79, sum.result(6.3, 1.3));
+  }
+
+  @Test
+  public void shouldReturnDirtySumFloat() {
+    assertEquals(24.9F, sum.result(2.5F, 6.4F));
+  }
+
 }
+
