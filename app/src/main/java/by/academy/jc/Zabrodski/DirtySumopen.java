4272924eab5dc98ebf848c218b717976a155f884
Artur Zabrodski <artur.zabrodski@gmail.com>
2021-08-15 11:47:04 +0300

    task 2

---
+++ app/src/main/java/by/academy/jc/Zabrodski/DirtySumopen.java
@@ -0,0 +1,22 @@
+package by.academy.jc.Zabrodski;
+
+class DirtySumopen {
+    /**
+    * @return - returns the sum of the values of the sum of numbers and the multiplication of numbers
+    */
+    public int result(int a, int b) {
+      return a + b + a * b;
+    }
+
+    public long result(long a, long b) {
+      return a + b + a * b;
+    }
+
+    public double result(double a, double b) {
+      return a + b + a * b;
+    }
+
+    public float result(float a, float b) {
+      return a + b + a * b;
+    }
+}
