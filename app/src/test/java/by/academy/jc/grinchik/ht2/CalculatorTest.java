15c0e1b8398b3c91b20028ac305b4b9c7e05b492
Alena Grinchik <lena.grinchik@gmail.com>
2021-08-15 19:16:52 +0300

    HW#2 Codacy fix

--- app/src/test/java/by/academy/jc/grinchik/ht2/CalculatorTest.java
+++ app/src/test/java/by/academy/jc/grinchik/ht2/CalculatorTest.java
@@ -7,12 +7,12 @@ import static org.junit.jupiter.api.Assertions.*;
 class CalculatorTest {
 
     @Test
-    void Should_Should_Return_19_When_Sum_3_And_4() {
+    void ShouldReturn19WhenSum3And4() {
         Calculator classUnderTest = new Calculator();
         assertEquals(19, classUnderTest.getSum(3, 4));
     }
     @Test
-    void Should_Return_0_When_Sum_0_And_0() {
+    void ShouldReturn0WhenSum0And0() {
         Calculator classUnderTest = new Calculator();
         assertEquals(0, classUnderTest.getSum(0, 0));
     }
