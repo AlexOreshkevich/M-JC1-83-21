dd0b92f24ae6cf6668b42c8634e004dc81ed046c
Artur Zabrodski <artur.zabrodski@gmail.com>
2021-08-15 08:43:10 +0300

    task 2. release v1.2

--- app/src/main/java/by/academy/jc/Zabrodski/ht2/DirtySumopen.java
+++ app/src/main/java/by/academy/jc/Zabrodski/ht2/DirtySumopen.java
@@ -4,19 +4,19 @@ public class DirtySumopen {
   /**
    * @return - returns the sum of the values of the sum of numbers and the multiplication of numbers
    */
-  public static int result(int a, int b) {
+  public int result(int a, int b) {
     return a + b + a * b;
   }
 
-  public static long result(long a, long b) {
+  public long result(long a, long b) {
     return a + b + a * b;
   }
 
-  public static double result(double a, double b){
+  public double result(double a, double b){
     return a + b + a * b;
   }
 
-  public static float result(float a, float b){
+  public float result(float a, float b){
     return a + b + a * b;
   }
 }
