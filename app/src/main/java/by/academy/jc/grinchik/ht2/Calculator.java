eb7d286cb157e9bf8297a6ef62abba515a405aee
Alena Grinchik <lena.grinchik@gmail.com>
2021-08-15 19:44:36 +0300

    HW#2 (add overloading method)

--- app/src/main/java/by/academy/jc/grinchik/ht2/Calculator.java
+++ app/src/main/java/by/academy/jc/grinchik/ht2/Calculator.java
@@ -4,4 +4,10 @@ public class Calculator {
      public int getSum(int a, int b) {
          return (a + b) + a * b;
      }
+     public float getSum(float a, float b) {
+         return (a + b) + a * b;
+     }
+     public double getSum(double a, double b) {
+         return (a + b) + a * b;
+     }
 }
--- app/src/test/java/by/academy/jc/grinchik/ht2/CalculatorTest.java
+++ app/src/test/java/by/academy/jc/grinchik/ht2/CalculatorTest.java
@@ -16,4 +16,14 @@ class CalculatorTest {
         Calculator classUnderTest = new Calculator();
         assertEquals(0, classUnderTest.getSum(0, 0));
     }
+    @Test
+    void shouldReturnValidResultWhenSumFloats() {
+        Calculator classUnderTest = new Calculator();
+        assertEquals(2.75f, classUnderTest.getSum(1.5f, 0.5f));
+    }
+    @Test
+    void shouldReturnValidResultWhenSumDoubles() {
+        Calculator classUnderTest = new Calculator();
+        assertEquals(2.75, classUnderTest.getSum(1.5, 0.5));
+    }
 }
