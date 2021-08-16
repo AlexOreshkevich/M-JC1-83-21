076ec0e162881a7e1eb383d233b1d7a20c84c86b
Aliaksandr Arashkevich <alexander.oreshkevich@gmail.com>
2021-08-16 11:52:38 +0300

    Commit hometask 2

---
+++ app/src/main/java/by/academy/jc/kzaikouskaya/ht2/Homework2.java
@@ -0,0 +1,28 @@
+package by.academy.jc.kzaikouskaya.ht2;
+
+import java.util.Scanner;
+
+import static java.lang.Integer.sum;
+
+public class Homework2 {
+
+    public static void main(String[] args) {
+
+        //input numbers
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input the first integer: ");
+        int a = in.nextInt();
+
+        System.out.print("Input the second integer: ");
+        int b = in.nextInt();
+
+        //sum of numbers
+        int sum = a + b;
+
+        //output the sum
+        System.out.println("Sum of numbers " + a + " and " + b + " is " + sum);
+
+    }
+
+}
+
