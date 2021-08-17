6b35bb6bcc06d093add70ca98afc232e0c4069d2
Dyskop <dimaskop18@gmail.com>
2021-08-17 15:30:26 +0300

    ht3/SKOPINOV

--- app/src/main/java/by/academy/jc/skopinov/ht3/Solution.java
+++ app/src/main/java/by/academy/jc/skopinov/ht3/Solution.java
@@ -3,17 +3,16 @@ package by.academy.jc.skopinov.ht3;
 class Solution {
 
   public int solution(int x, int y, int d){
-    int jumps;
     int distance = y - x;
     if (x <= 0 || y<= 0 || x > 1000000000 || y > 1000000000 || d > 1000000000)
       throw new IllegalArgumentException("parameter values don't correspond to the task condition");
     else if (d <= 0)
       throw new IllegalArgumentException("the small frog jumps backward or upward and never reaches the goal");
     else if (x >= y)
-      return jumps = 0;
+      return 0;
     else if (distance % d == 0)
-      return jumps = distance / d;
-    else return jumps = distance / d + 1;
+      return distance / d;
+    else return distance / d + 1;
   }
 }
 
--- app/src/test/java/by/academy/jc/skopinov/ht3/SolutionTest.java
+++ app/src/test/java/by/academy/jc/skopinov/ht3/SolutionTest.java
@@ -6,54 +6,60 @@ import org.junit.jupiter.api.Test;
 
 class SolutionTest {
 
+  // random parameter values test
   @Test
-  void random_parameter_values() throws IllegalArgumentException {
+  void test1() throws IllegalArgumentException {
     Solution test = new Solution();
     assertEquals(3, test.solution(10,85,30));
   }
 
+  // x and y are equal
   @Test
-  void x_and_y_are_equal() throws IllegalArgumentException {
+  void test2() throws IllegalArgumentException {
     Solution test = new Solution();
     assertEquals(0, test.solution(1,1,1000000000));
   }
 
+  // x greater than y
   @Test
-  void x_greater_than_y() throws IllegalArgumentException {
+  void test3() throws IllegalArgumentException {
     Solution test = new Solution();
     assertEquals(0, test.solution(1000000000,1,1));
   }
 
+  // distance is a multiple of jump length
   @Test
-  void distance_is_a_multiple_of_jump_length() throws IllegalArgumentException {
+  void test4() throws IllegalArgumentException {
     Solution test = new Solution();
     assertEquals(3, test.solution(100,1000,300));
   }
 
+  // extreme parameter values 1
   @Test
-  void extreme_parameter_values1() throws IllegalArgumentException {
+  void test5() throws IllegalArgumentException {
     Solution test = new Solution();
     assertEquals(0, test.solution(1,1,1));
   }
 
+  // extreme parameter values 2
   @Test
-  void extreme_parameter_values2() throws IllegalArgumentException {
+  void test6() throws IllegalArgumentException {
     Solution test = new Solution();
     assertEquals(0, test.solution(1000000000,1000000000,1000000000));
   }
 
+  // extreme parameter values 3
   @Test
-  void extreme_parameter_values3() throws IllegalArgumentException {
+  void test7() throws IllegalArgumentException {
     Solution test = new Solution();
     assertEquals(999999999, test.solution(1,1000000000,1));
   }
 
+  // extreme parameter values 4
   @Test
-  void extreme_parameter_values4() throws IllegalArgumentException {
+  void test8() throws IllegalArgumentException {
     Solution test = new Solution();
     assertEquals(1, test.solution(1,1000000000,1000000000));
   }
-
-
 
 }
