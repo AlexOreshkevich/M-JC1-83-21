5bb763cd8962f504816270462e2377e40b51292d
Uladzislau <Klimaxovich@mail.ru>
2021-08-18 16:19:39 +0300

    HT3/Klimakhovich

---
+++ app/src/test/java/by/academy/jc/klimakhovich_ht3/TestFrogJumper.java
@@ -0,0 +1,49 @@
+package by.academy.jc.klimakhovich_ht3;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestFrogJumper {
+    FrogJumper jump = new FrogJumper();
+
+
+    @Test
+    public void numOfJumps() {
+        assertEquals(3, jump.frog1(10, 85, 30), "should return 3 jumps");
+    }
+
+    @Test
+    public void numOfMillionJumps1() {
+        assertEquals(65555556, jump.frog2(33333334, 2000000000, 30), "should return 65555556 jumps");
+    }
+
+    @Test
+    public void numOfThousandsJumps2() {
+        assertEquals(655556, jump.frog3(33333334, 2000000000, 3000), "should return 655556 jumps");
+    }
+    @Test
+    public void null_Y_Jumps1() {
+        assertEquals(0, jump.frog1(1, 0, 1), "should return 0 jumps");
+    }
+    @Test
+    public void null_Y_Jumps2() {
+        assertEquals(0, jump.frog2(1, 0, 1), "should return 0 jumps");
+    }
+    @Test
+    public void null_Y_Jumps3() {
+        assertEquals(0, jump.frog3(1, 0, 1), "should return 0 jumps");
+    }
+    @Test
+    public void null_X_Jumps1() {
+        assertEquals(2, jump.frog1(0, 1, 1), "should return 0 jumps");
+    }
+    @Test
+    public void null_X_Jumps2() {
+        assertEquals(2, jump.frog2(0, 1, 1), "should return 0 jumps");
+    }
+    @Test
+    public void null_X_Jumps3() {
+        assertEquals(2, jump.frog3(0, 1, 1), "should return 0 jumps");
+    }
+}
