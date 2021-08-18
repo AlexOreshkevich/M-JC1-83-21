4790b2a93a7bf33412624226440ce984b15f818f
TimofeiShorokhov <limjkeee@mail.ru>
2021-08-18 15:03:20 +0300

    Ht3/shorokhov

--- app/src/main/java/by/academy/jc/shorokhov/frog/Frog.java
+++ app/src/main/java/by/academy/jc/shorokhov/frog/Frog.java
@@ -2,12 +2,12 @@ package by.academy.jc.shorokhov.frog;
 
 public class Frog {
 
-    public int HugeSolution(int x, int y, int d) {
+    public int solution(int x, int y, int d) {
         int jump = 0;
         int i = x;
         for ( i = x; i < y; i = i + d ) {
             if (x > 1000000000 || y > 1000000000) {
-                boolean b = x <= y;
+                boolean bigger = x <= y;
             }
             jump++;
         }
