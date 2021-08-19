43bba9432e43ff11eeebc01e36464314c0549cfb
Natalia-Dubautsova <natagromova.99@mail.ru>
2021-08-19 10:06:13 +0300

    4th homework

---
+++ app/src/main/java/by/academy/jc/dubautsova/FilterString.java
@@ -0,0 +1,8 @@
+package by.academy.jc.dubautsova;
+
+public class FilterString {
+    public static String filterContent(String line){
+        String lineOutput = line.replaceAll("<.*?>", "");
+        return lineOutput;
+    }
+}
---
+++ app/src/test/java/by/academy/jc/Dubautsova/FilterStringTest.java
@@ -0,0 +1,15 @@
+package by.academy.jc.Dubautsova;
+
+import by.academy.jc.dubautsova.FilterString;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FilterStringTest {
+    @Test
+    public void shouldRemoveTags(){
+        assertEquals("Hello, world!", FilterString.filterContent("<h1>Hello, world!</h1>"));
+        assertEquals("Hello, world!!", FilterString.filterContent("<h1><p>Hello, world!</p></h1>"));
+        assertEquals("Hello, world!!!", FilterString.filterContent("<p>Hello, world!</p>"));
+    }
+}
