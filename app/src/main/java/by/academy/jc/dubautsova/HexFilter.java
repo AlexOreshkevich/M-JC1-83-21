6a13aed9586964220c24af3b130f884020eb921d
Natalia-Dubautsova <natagromova.99@mail.ru>
2021-08-25 13:03:18 +0300

    ht4new/dubautsova fixed

--- app/src/main/java/by/academy/jc/dubautsova/HexFilter.java
+++ app/src/main/java/by/academy/jc/dubautsova/HexFilter.java
@@ -2,7 +2,6 @@ package by.academy.jc.dubautsova;
 
 public class HexFilter {
     public static String withoutHexNumbers(String line){
-        line = line.replaceAll("0[xX][0-9A-F]+", "");
-        return line;
+        return line.replaceAll("0[xX][0-9A-F]+", "");
     }
 }
