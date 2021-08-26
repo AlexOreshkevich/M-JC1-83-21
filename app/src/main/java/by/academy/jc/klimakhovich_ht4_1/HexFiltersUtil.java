8c28a98a96d10dac8d1628130bd427c5ac0d757f
Uladzislau Klimakhovich <60568894+Vladonblch@users.noreply.github.com>
2021-08-26 09:45:15 +0300

    Ht4_1/Klimakhovich (#123)

---
+++ app/src/main/java/by/academy/jc/klimakhovich_ht4_1/HexFiltersUtil.java
@@ -0,0 +1,23 @@
+package by.academy.jc.klimakhovich_ht4_1;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class HexFiltersUtil {
+
+    public static String filter(String source){
+
+        String number16 = " 0[xX][0-9a-fA-F]+";
+
+        Pattern pattern = Pattern.compile(number16);
+        Matcher matcher = pattern.matcher(source);
+
+        String result = source;
+
+        while (matcher.find()){
+            result = result.replaceAll(matcher.group(), "");
+
+        }
+        return result;
+    }
+}
