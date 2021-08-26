999c034f5bee1bd7093b739123e19cab2062f647
Dyskop <dimaskop18@gmail.com>
2021-08-26 05:04:43 +0300

    ht4/SKOPINOV

---
+++ app/src/main/java/by/academy/jc/skopinov/ht4/DataInput.java
@@ -0,0 +1,33 @@
+package by.academy.jc.skopinov.ht4;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class DataInput {
+
+  public static void main(String[] args) {
+    ArrayList<String> lines = new ArrayList<>();
+    int allLinesLength = 0;
+    Scanner scanner = new Scanner(System.in);
+
+    int n = scanner.nextInt();
+    if (n < 1 || n > 100) {
+      throw new IllegalArgumentException();
+    }
+
+    for (int i = 0; i <= n; i++) {
+      String line = scanner.nextLine();
+      allLinesLength += line.length();
+      if (line.length() > 10E4)
+        throw new IllegalArgumentException();
+      lines.add(line);
+    }
+
+    if (allLinesLength > 10E6)
+      throw new IllegalArgumentException();
+
+    for (int i = 0; i < n; i++) {
+      System.out.println(TagsSolution.extractingContent(lines.get(i)));
+    }
+  }
+}
---
+++ app/src/main/java/by/academy/jc/skopinov/ht4/TagsSolution.java
@@ -0,0 +1,74 @@
+package by.academy.jc.skopinov.ht4;
+
+
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+public class TagsSolution {
+  public static String extractingContent(String s) {
+    if (s == null)
+      throw new IllegalArgumentException();
+    String result;
+    result = firstCheck(s);
+    if(!result.equals("None")) {
+      result = secondCheck(s);
+    }
+    return result;
+  }
+
+  public static String firstCheck(String s){
+    Pattern pattern = Pattern.compile("<[a-zA-Z][.[^<>]]*?>[.[^<]]+?</[a-zA-Z][.[^<>]]*?>");
+    Matcher matcher = pattern.matcher(s);
+    long count = matcher.results().count();
+    if (count == 0) {
+      return "None";
+    }
+    else return "";
+  }
+
+  public static String secondCheck(String s){
+    String positiveResult = null;
+
+    String tagText;
+    String openingTagValue = "";
+    String closingTagValue = "";
+
+    Pattern p = Pattern.compile("<[a-zA-Z][.[^<>]]*?>[.[^<]]+?</[a-zA-Z][.[^<>]]*?>");
+    Matcher m = p.matcher(s);
+    if (m.find()) {
+      int start = m.start();
+      int end = m.end();
+      tagText = s.substring(start, end);
+
+      Pattern openingtag = Pattern.compile("<[a-zA-Z][.[^<>]]*?>");
+      Matcher mOpeningtag = openingtag.matcher(tagText);
+      if (mOpeningtag.find()) {
+        int startOpen = mOpeningtag.start();
+        int endOpen = mOpeningtag.end();
+        openingTagValue = tagText.substring(startOpen, endOpen);
+      }
+
+      Pattern closingtag = Pattern.compile("</[a-zA-Z][.[^<>]]*?>");
+      Matcher mClosingtag = closingtag.matcher(tagText);
+      if (mClosingtag.find()) {
+        int startClose = mClosingtag.start();
+        int endClose = mClosingtag.end();
+        closingTagValue = tagText.substring(startClose, endClose);
+      }
+
+      closingTagValue = closingTagValue.replaceFirst("/", "");
+
+      if (closingTagValue.equals(openingTagValue)) {
+        System.out.println(tagText.replaceAll("</?[a-zA-Z][.[^<>]]*?>", ""));
+        positiveResult = tagText.replaceAll("</?[a-zA-Z][.[^<>]]*?>", "");
+      }
+      else positiveResult = "None";
+    }
+    return positiveResult;
+  }
+}
+
+
+
