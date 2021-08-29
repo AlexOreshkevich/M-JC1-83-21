1aec42d7c7165ef52d77180c177e42701ea176d0
Dyskop <dimaskop18@gmail.com>
2021-08-29 11:55:14 +0300

    ht5/SKOPINOV

---
+++ app/src/main/java/by/academy/jc/skopinov/ht5/AnagramUtils.java
@@ -0,0 +1,45 @@
+package by.academy.jc.skopinov.ht5;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class AnagramUtils {
+
+  public static boolean isAnagram(String a, String b) {
+    if (a.length() > 50 || b.length() > 50)
+      throw new IllegalArgumentException();
+    Pattern pattern = Pattern.compile("[.[^a-zA-Z]]+?");
+    Matcher matcherA = pattern.matcher(a);
+    Matcher matcherB = pattern.matcher(b);
+    if (matcherA.find() || matcherB.find())
+      throw new IllegalArgumentException();
+
+    String aLowerCase = a.toLowerCase();
+    String bLowerCase = b.toLowerCase();
+    ArrayList<Character> aArray = new ArrayList<>();
+    ArrayList<Character> bArray = new ArrayList<>();
+    for (char c : aLowerCase.toCharArray()) {
+      aArray.add(c);
+    }
+    for (char c : bLowerCase.toCharArray()) {
+      bArray.add(c);
+    }
+    Collections.sort(aArray);
+    Collections.sort(bArray);
+
+    StringBuilder a1 = new StringBuilder();
+    for (char c : aArray) {
+      a1.append(c);
+    }
+    StringBuilder b1 = new StringBuilder();
+    for (char c : bArray) {
+      b1.append(c);
+    }
+    String sortedALowerCase = a1.toString();
+    String sortedBLowerCase = b1.toString();
+
+    return sortedALowerCase.equals(sortedBLowerCase);
+  }
+}
