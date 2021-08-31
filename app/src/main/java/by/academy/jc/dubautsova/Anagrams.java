39bfd5d34d86b2209e11a2b0e0812219ab75c74a
Natalia-Dubautsova <natagromova.99@mail.ru>
2021-08-31 10:11:43 +0300

    ht4new/dubautsova fixed new

--- app/src/main/java/by/academy/jc/dubautsova/Anagrams.java
+++ app/src/main/java/by/academy/jc/dubautsova/Anagrams.java
@@ -7,14 +7,14 @@ import static java.util.Arrays.sort;
 
 public class Anagrams {
 
-    public static String isAnagram(String a, String b){
+    public static boolean isAnagram(String a, String b){
         String a1 = a.toLowerCase();
         String b1 = b.toLowerCase();
         char[] chA1 = a1.toCharArray();
         char[] chB1 = b1.toCharArray();
         sort(chA1);
         sort(chB1);
-        if (Arrays.equals(chA1, chB1)) return "Anagrams";
-        else return "Not Anagrams";
+        if (Arrays.equals(chA1, chB1)) return (Arrays.equals(chA1, chB1));
+        else return false;
     }
 }
--- app/src/test/java/by/academy/jc/dubautsova/AnagramsTest.java
+++ app/src/test/java/by/academy/jc/dubautsova/AnagramsTest.java
@@ -7,14 +7,22 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class AnagramsTest {
     @Test
     public void shouldShowAreTheWordsAnagrams(){
-        assertEquals("Anagrams", Anagrams.isAnagram("anagram", "margana"));
+        assertEquals(true, Anagrams.isAnagram("anagram", "margana"));
     }
     @Test
     public void shouldShowAreTheWordsAnagrams1(){
-        assertEquals("Not Anagrams", Anagrams.isAnagram("anagramm", "marganaa"));
+        assertEquals(false, Anagrams.isAnagram("anagramm", "marganaa"));
     }
     @Test
     public void shouldShowAreTheWordsAnagrams2(){
-        assertEquals("Anagrams", Anagrams.isAnagram("Hello", "hello"));
+        assertEquals(true, Anagrams.isAnagram("Hello", "hello"));
+    }
+    @Test
+    public void shouldShowAreTheWordsAnagrams3(){
+        assertEquals(true, Anagrams.isAnagram("CaT", "Act"));
+    }
+    @Test
+    public void shouldShowAreTheWordsAnagrams4(){
+        assertEquals(false, Anagrams.isAnagram("Helloooooo", "hello"));
     }
 }
