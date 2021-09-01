8dfb23d2fd9ce86b17c43a072145b30ba3d63454
Dmitriy Skopinov <88438659+Dyskop@users.noreply.github.com>
2021-09-01 20:14:28 +0300

    ht5/SKOPINOV (#151)

---
+++ app/src/test/java/by/academy/jc/skopinov/ht5/AnagramUtilsTest.java
@@ -0,0 +1,48 @@
+package by.academy.jc.skopinov.ht5;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AnagramUtilsTest {
+
+  @Test
+  void case1() throws IllegalArgumentException {
+    assertTrue(AnagramUtils.isAnagram("anagram", "margana"));
+  }
+
+  @Test
+  void case2() throws IllegalArgumentException {
+    assertFalse(AnagramUtils.isAnagram("anagramm", "marganaa"));
+  }
+
+  @Test
+  void case3() throws IllegalArgumentException {
+    assertTrue(AnagramUtils.isAnagram("Hello", "hello"));
+  }
+
+  @Test
+  void oneLetter() throws IllegalArgumentException {
+    assertTrue(AnagramUtils.isAnagram("A", "a"));
+  }
+
+  @Test
+  void fiftyDifferentLetters() throws IllegalArgumentException {
+    assertFalse(AnagramUtils.isAnagram("jklajefGJkgHFjHGjhkjGkjgkjggKJglkjhLJKgjhHJHjgLgdQ", "jKfghgFjGJHghjGfJHFHJJHGjjhgjhgjHGGhhlfDduLffHkFjk"));
+  }
+
+  @Test
+  void twentyIdenticalLettersOfDifferentRegister() throws IllegalArgumentException {
+    assertTrue(AnagramUtils.isAnagram("UUuuUuUuUUKhhkhhkhkk", "uuuuUuuUuuKKKhkHkHHH"));
+  }
+
+  @Test
+  void wrongInputFiftyOneLetters() {
+    assertThrows(IllegalArgumentException.class, ()-> AnagramUtils.isAnagram("jklajefGJkgHFjHGjhkjGkjgkjggKJglkjhLJKgjhHJHjgLgdQw", "jKfghgFjGJHghjGfJHFHJJHGjjhgjhgjHGGhhlfDduLffHkFjkP"));
+  }
+
+  @Test
+  void wrongInputInvalidCharacter() {
+    assertThrows(IllegalArgumentException.class, ()-> AnagramUtils.isAnagram("hello_world", "hello_world"));
+  }
+}
