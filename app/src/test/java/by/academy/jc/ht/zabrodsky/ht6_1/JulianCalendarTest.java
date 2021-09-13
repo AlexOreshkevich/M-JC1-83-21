d0cc207f77c6611a687d842d34ba09c2167ee30e
Artur Zabrodski <artur.zabrodski@gmail.com>
2021-09-13 04:17:52 +0300

    ht6-1/zabrodsky

--- app/src/test/java/by/academy/jc/ht/zabrodsky/ht6_1/JulianCalendarTest.java
+++ app/src/test/java/by/academy/jc/ht/zabrodsky/ht6_1/JulianCalendarTest.java
@@ -10,70 +10,143 @@ import org.junit.jupiter.api.Test;
 public class JulianCalendarTest {
 
   @Test
-  public void addingTheDateAndTime_1() {
-    JulianCalendar myCalendar1 = new JulianCalendar(2020, 5, 20, 22, 30);
-    assertEquals(LocalDateTime.of(2020, 5, 7, 22, 30), myCalendar1.getDateTime());
-    assertEquals(LocalDate.of(2020, 5, 7), myCalendar1.getDate());
-    assertEquals(LocalTime.of(22, 30), myCalendar1.getTime());
+  public void addingTheDateAndTime1() {
+    JulianCalendar myCalendar = new JulianCalendar(2020, 5, 20, 22, 30);
+    assertEquals(LocalDateTime.of(2020, 5, 7, 22, 30), myCalendar.getDateTime());
+    assertEquals(LocalDate.of(2020, 5, 7), myCalendar.getDate());
+    assertEquals(LocalTime.of(22, 30), myCalendar.getTime());
   }
 
   @Test
-  public void addingTheDateAndTime_2() {
-    JulianCalendar myCalendar2 = new JulianCalendar(2021, 9, 12);
-    assertEquals(LocalDate.of(2021, 8, 30), myCalendar2.getDate());
+  public void addingTheDateAndTime2() {
+    JulianCalendar myCalendar = new JulianCalendar(2021, 9, 12);
+    assertEquals(LocalDate.of(2021, 8, 30), myCalendar.getDate());
 
-    myCalendar2.setDate(2017, 7, 26);
-    assertEquals(LocalDate.of(2017, 7, 13), myCalendar2.getDate());
-    assertEquals(LocalDateTime.of(2017, 7, 13, 0, 0), myCalendar2.getDateTime());
+    myCalendar.setDate(2017, 7, 26);
+    assertEquals(LocalDate.of(2017, 7, 13), myCalendar.getDate());
+    assertEquals(LocalDateTime.of(2017, 7, 13, 0, 0), myCalendar.getDateTime());
 
   }
 
   @Test
-  public void addingTheDate_1() {
-    JulianCalendar myCalendar3 = new JulianCalendar();
-    myCalendar3.setDate(1582, 10, 15);
-    assertEquals(LocalDate.of(1582, 10, 5), myCalendar3.getDate());  // -10
+  public void addingTheDateAndTime3() {
+    JulianCalendar myCalendar = new JulianCalendar(2021, 9, 13,10,40);
+    assertEquals(LocalTime.of(10, 40), myCalendar.getTime());
+    myCalendar.setTime(11,50);
+    assertEquals(LocalDateTime.of(2021, 8, 31, 11, 50), myCalendar.getDateTime());
+  }
+
+
+
+  @Test
+  public void addingTheDate1() {
+    JulianCalendar myCalendar = new JulianCalendar();
+    myCalendar.setDate(1582, 10, 15);
+    assertEquals(LocalDate.of(1582, 10, 5), myCalendar.getDate());  // -10
   }
 
   @Test
-  public void addingTheDate_2() {
-    JulianCalendar myCalendar4 = new JulianCalendar();
-    myCalendar4.setDate(1700, 3, 12);
-    assertEquals(LocalDate.of(1700, 3, 1), myCalendar4.getDate());  // -11
+  public void addingTheDate2() {
+    JulianCalendar myCalendar = new JulianCalendar();
+    myCalendar.setDate(1700, 3, 12);
+    assertEquals(LocalDate.of(1700, 3, 1), myCalendar.getDate());  // -11
   }
 
   @Test
-  public void addingTheDate_3() {
-    JulianCalendar myCalendar5 = new JulianCalendar();
-    myCalendar5.setDate(1850, 9, 3);
-    assertEquals(LocalDate.of(1850, 8, 22), myCalendar5.getDate());  // -12
+  public void addingTheDate3() {
+    JulianCalendar myCalendar = new JulianCalendar();
+    myCalendar.setDate(1850, 9, 3);
+    assertEquals(LocalDate.of(1850, 8, 22), myCalendar.getDate());  // -12
   }
 
   @Test
-  public void addingTheDate_4() {
-    JulianCalendar myCalendar6 = new JulianCalendar();
-    myCalendar6.setDate(1950, 1, 7);
-    assertEquals(LocalDate.of(1949, 12, 25), myCalendar6.getDate());  // -13
+  public void addingTheDate4() {
+    JulianCalendar myCalendar = new JulianCalendar();
+    myCalendar.setDate(1950, 1, 7);
+    assertEquals(LocalDate.of(1949, 12, 25), myCalendar.getDate());  // -13
   }
 
   @Test
-  public void addingTheDate_5() {
-    JulianCalendar myCalendar7 = new JulianCalendar();
-    myCalendar7.setDate(2200, 3, 15);
-    assertEquals(LocalDate.of(2200, 3, 1), myCalendar7.getDate());  // -14
+  public void addingTheDate5() {
+    JulianCalendar myCalendar = new JulianCalendar();
+    myCalendar.setDate(2200, 3, 15);
+    assertEquals(LocalDate.of(2200, 3, 1), myCalendar.getDate());  // -14
   }
 
   @Test
-  public void addingTheDate_6() {
-    JulianCalendar myCalendar8 = new JulianCalendar();
-    myCalendar8.setDate(2200, 3, 16);
-    assertEquals(LocalDate.of(2200, 3, 1), myCalendar8.getDate());  // -15
+  public void addingTheDate6() {
+    JulianCalendar myCalendar = new JulianCalendar();
+    myCalendar.setDate(2200, 3, 16);
+    assertEquals(LocalDate.of(2200, 3, 1), myCalendar.getDate());  // -15
   }
 
   @Test
-  public void addingTheDate_7() {
-    JulianCalendar myCalendar9 = new JulianCalendar();
-    myCalendar9.setDate(2400, 1, 1);
-    assertEquals(LocalDate.of(2399, 12, 16), myCalendar9.getDate());  // -16
+  public void addingTheDate7() {
+    JulianCalendar myCalendar = new JulianCalendar();
+    myCalendar.setDate(2400, 1, 1);
+    assertEquals(LocalDate.of(2399, 12, 16), myCalendar.getDate());  // -16
+  }
+
+  @Test
+  public void addingTheDateTime1() {
+    JulianCalendar myCalendar = new JulianCalendar();
+    myCalendar.setDateTime(1582, 10, 15,1,1);
+    assertEquals(LocalDateTime.of(1582, 10, 5,1,1), myCalendar.getDateTime());  // -10
+  }
+
+  @Test
+  public void addingTheDateTime2() {
+    JulianCalendar myCalendar = new JulianCalendar();
+    myCalendar.setDateTime(1700, 3, 12,1,1);
+    assertEquals(LocalDateTime.of(1700, 3, 1,1,1), myCalendar.getDateTime());  // -11
+  }
+
+  @Test
+  public void addingTheDateTime3() {
+    JulianCalendar myCalendar = new JulianCalendar();
+    myCalendar.setDateTime(1850, 9, 3,1,1);
+    assertEquals(LocalDateTime.of(1850, 8, 22,1,1), myCalendar.getDateTime());  // -12
+  }
+
+  @Test
+  public void addingTheDateTime4() {
+    JulianCalendar myCalendar = new JulianCalendar();
+    myCalendar.setDateTime(1950, 1, 7,1,1);
+    assertEquals(LocalDateTime.of(1949, 12, 25,1,1), myCalendar.getDateTime());  // -13
+  }
+
+  @Test
+  public void addingTheDateTime5() {
+    JulianCalendar myCalendar = new JulianCalendar();
+    myCalendar.setDateTime(2200, 3, 15,1,1);
+    assertEquals(LocalDateTime.of(2200, 3, 1,1,1), myCalendar.getDateTime());  // -14
+  }
+
+  @Test
+  public void addingTheDateTime6() {
+    JulianCalendar myCalendar = new JulianCalendar();
+    myCalendar.setDateTime(2200, 3, 16,1,1);
+    assertEquals(LocalDateTime.of(2200, 3, 1,1,1), myCalendar.getDateTime());  // -15
+  }
+
+  @Test
+  public void addingTheDateTime7() {
+    JulianCalendar myCalendar = new JulianCalendar();
+    myCalendar.setDateTime(2400, 1, 1,1,1);
+    assertEquals(LocalDateTime.of(2399, 12, 16,1,1), myCalendar.getDateTime());  // -16
+  }
+
+  @Test
+  public void addingTheDateTime8() {
+    JulianCalendar myCalendar = new JulianCalendar();
+    myCalendar.setDateTime(1400, 1, 1,1,1);
+    assertEquals(null, myCalendar.getDateTime());
+  }
+
+  @Test
+  public void addingTheDate8() {
+    JulianCalendar myCalendar = new JulianCalendar();
+    myCalendar.setDate(1400, 1, 1);
+    assertEquals(null, myCalendar.getDate());
   }
 }
