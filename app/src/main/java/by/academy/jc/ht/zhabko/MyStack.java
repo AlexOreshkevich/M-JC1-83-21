1b738dcdffbc6d280d65992958266ba13c05bf5d
Alexey_Zhabko <zhabkoag@gmail.com>
2021-09-23 18:11:33 +0300

    MyStack core methods implemented

--- app/src/main/java/by/academy/jc/ht/zhabko/MyStack.java
+++ app/src/main/java/by/academy/jc/ht/zhabko/MyStack.java
@@ -1,7 +1,9 @@
 package by.academy.jc.ht.zhabko;
 
 
-public class MyStack <E> {
+import java.util.*;
+
+public class MyStack <E> implements List {
 
     private Node <E> first;
     private int size;
@@ -18,13 +20,234 @@ public class MyStack <E> {
         }
     }
 
-    public int getSize() {
+
+    @Override
+    public int size() {
         return size;
     }
 
     public boolean isEmpty() {
-        return size==0;
+        return size == 0;
     }
+
+    @Override
+    public boolean contains(Object o) {
+
+        for (Node <E> x = first; x!=null; x = x.next){
+            if (o == null & x.item == null) {
+                return true;
+            }
+            if (x.item == null) continue;
+            if (o !=null & x.item.equals(o)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public Iterator iterator() {
+        return new Itr();
+    }
+
+    private class Itr implements Iterator <E> {
+
+        int currentIndex = 0;
+
+        Node <E> currentNode = first;
+
+        @Override
+        public boolean hasNext() {
+            return currentIndex != size;
+        }
+
+        @Override
+        public E next() {
+            if (!hasNext())
+                throw new NoSuchElementException();
+            currentIndex++;
+
+            if (currentNode.next == null) {
+                return currentNode.item;
+            } else {
+                currentNode = currentNode.next;
+            }
+            return currentNode.prev.item;
+        }
+    }
+
+    @Override
+    public Object[] toArray() {
+        Object[] objects = new Object[size];
+        int counter = 0;
+        for (Node <E> x = first; x!=null; x = x.next) {
+            objects[counter] = x.item;
+            counter++;
+        }
+        return objects;
+    }
+
+    @Override
+    public boolean add(Object o) {
+        push((E) o);
+        return peek()==o ? true : false;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+
+        Node <E> nextLink;
+        Node <E> prevLink;
+
+        for (Node <E> x = first; x!=null; x = x.next){
+            if (o == null & x.item == null) {
+                prevLink = x.prev;
+                nextLink = x.next;
+                x.prev.next = nextLink;
+                x.next.prev = prevLink;
+                return true;
+            }
+            if (x.item == null) continue;
+            if (o !=null & x.item.equals(o)) {
+                prevLink = x.prev;
+                nextLink = x.next;
+                x.prev.next = nextLink;
+                x.next.prev = prevLink;
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+
+
+    @Override
+    public void clear() {
+        for (Node <E> x = first; x!=null; x = x.next){
+            pop();
+            size--;
+        }
+
+    }
+
+    @Override
+    public Object get(int index) {
+
+        if (index >= size) {
+            throw new RuntimeException("Index out of range");
+        }
+
+        int counter = 0;
+        Node <E> found = null;
+
+        for (Node <E> x = first; x!=null; x = x.next){
+            if (counter == index) {
+                found = x;
+                break;
+            }
+            counter++;
+        }
+
+        return found.item;
+    }
+
+    @Override
+    public Object set(int index, Object element) {
+        Node <E> found = null;
+        if (index >= size) {
+            throw new RuntimeException("Index out of range");
+        }
+
+        int counter = 0;
+
+
+        for (Node <E> x = first; x!=null; x = x.next){
+            if (counter == index) {
+                found = new Node<>(x.prev,x.item,x.next);
+                x.item = (E) element;
+                break;
+            }
+            counter++;
+        }
+
+        return found.item;
+    }
+
+    @Override
+    public void add(int index, Object element) {
+
+        if (index >= size) {
+            throw new RuntimeException("Index out of range");
+        }
+
+        int counter = 0;
+
+
+        for (Node <E> x = first; x!=null; x = x.next){
+            if (counter == index) {
+
+                Node <E> newNode = new Node<>(x.prev, (E) element,x);
+
+                x.prev.next = newNode;
+                x.prev = newNode;
+
+                size++;
+                break;
+            }
+            counter++;
+        }
+
+    }
+
+    @Override
+    public Object remove(int index) {
+
+        if (index >= size) {
+            throw new RuntimeException("Index out of range");
+        }
+
+        int counter = 0;
+        E found = null;
+
+
+        for (Node <E> x = first; x!=null; x = x.next) {
+            if (counter == index) {
+
+                x.prev.next = x.next;
+                x.next.prev = x.prev;
+
+                found = x.item;
+                size--;
+            }
+        }
+
+        return found;
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        int counter = 0;
+
+        for (Node <E> x = first; x!=null; x = x.next){
+            if (o == null & x.item == null) {
+                return counter;
+            }
+            if (x.item == null) {
+                counter++;
+                continue;
+            };
+            if (o !=null & x.item.equals(o)) {
+                return counter;
+            }
+            counter++;
+        }
+
+        return -1;
+    }
+
+
+
 
     private void linkFirst(E e) {
         final Node<E> f = first;
@@ -36,9 +259,9 @@ public class MyStack <E> {
         size++;
     }
 
-   E peek() throws Exception {
+   E peek() throws RuntimeException {
        if (first==null)
-           throw new  Exception("Current MyStack is empty");
+           throw new  RuntimeException("Current MyStack is empty");
         return first.item;
     }
 
@@ -46,9 +269,63 @@ public class MyStack <E> {
         linkFirst(e);
     }
 
-    void pop() throws Exception {
+    void pop() throws RuntimeException {
         if (first==null)
-            throw new Exception("Current MyStack is empty");
+            throw new RuntimeException("Current MyStack is empty");
         first=first.next;
+    }
+
+
+
+
+    //Not implemented
+    @Override
+    public ListIterator listIterator() {
+        return null;
+    }
+
+    @Override
+    public ListIterator listIterator(int index) {
+        return null;
+    }
+
+    @Override
+    public List subList(int fromIndex, int toIndex) {
+        return null;
+    }
+
+    @Override
+    public boolean retainAll(Collection c) {
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(Collection c) {
+        return false;
+    }
+
+    @Override
+    public boolean containsAll(Collection c) {
+        return false;
+    }
+
+    @Override
+    public Object[] toArray(Object[] a) {
+        return new Object[0];
+    }
+
+    @Override
+    public boolean addAll(Collection c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(int index, Collection c) {
+        return false;
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        return 0;
     }
 }
--- app/src/test/java/by/academy/jc/ht/zhabko/MyStackTest.java
+++ app/src/test/java/by/academy/jc/ht/zhabko/MyStackTest.java
@@ -2,12 +2,16 @@ package by.academy.jc.ht.zhabko;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class MyStackTest {
 
     @Test
-    void shouldPushAllIntegerObjects() throws Exception {
+    void shouldPushAllIntegerObjects() throws RuntimeException {
         MyStack <Integer> testObject = new MyStack<>();
         testObject.push(1);
         testObject.push(2);
@@ -18,7 +22,7 @@ public class MyStackTest {
     }
 
     @Test
-    void shouldPushAllStringObjects() throws Exception {
+    void shouldPushAllStringObjects() throws RuntimeException {
         MyStack <String> testObject = new MyStack<>();
         testObject.push("Test");
         testObject.push("Java");
@@ -36,11 +40,11 @@ public class MyStackTest {
         testObject.push(3);
         testObject.push(4);
         testObject.push(5);
-        assertEquals(5,testObject.getSize());
+        assertEquals(5,testObject.size());
     }
 
     @Test
-    void shouldPopAndReturnIntegerObject() throws Exception {
+    void shouldPopAndReturnIntegerObject() throws RuntimeException {
         MyStack <Integer> testObject = new MyStack<>();
         testObject.push(1);
         testObject.push(2);
@@ -52,7 +56,7 @@ public class MyStackTest {
     }
 
     @Test
-    void shouldPopAndReturnStringObject() throws Exception {
+    void shouldPopAndReturnStringObject() throws RuntimeException {
         MyStack <String> testObject = new MyStack<>();
         testObject.push("Test");
         testObject.push("Java");
@@ -79,7 +83,7 @@ public class MyStackTest {
     @Test
     void shouldThrowRuntimeExceptionOnPeekOnEmptyMyStack() {
         MyStack <Integer> testObject = new MyStack<>();
-        Exception exception = assertThrows(Exception.class, testObject::peek);
+        Exception exception = assertThrows(RuntimeException.class, testObject::peek);
         String actualMessage = exception.getMessage();
         String expectedMessage = "Current MyStack is empty";
         assertEquals(expectedMessage,actualMessage);
@@ -88,9 +92,310 @@ public class MyStackTest {
     @Test
     void shouldThrowRuntimeExceptionOnPoPOnEmptyMyStack() {
         MyStack <Integer> testObject = new MyStack<>();
-        Exception exception = assertThrows(Exception.class, testObject::pop);
+        Exception exception = assertThrows(RuntimeException.class, testObject::pop);
         String actualMessage = exception.getMessage();
         String expectedMessage = "Current MyStack is empty";
         assertEquals(expectedMessage,actualMessage);
     }
+
+    @Test
+    void shouldReturnZeroSumOfStackWithIterator() {
+        MyStack <Integer> testObject = new MyStack<>();
+        int testSum = 28;
+        testObject.push(1);
+        testObject.push(2);
+        testObject.push(3);
+        testObject.push(4);
+        testObject.push(5);
+        testObject.push(6);
+        testObject.push(7);
+
+        Iterator <Integer> iterator = testObject.iterator();
+
+        while (iterator.hasNext()) {
+            testSum -= iterator.next();
+        }
+        assertEquals(0,testSum);
+    }
+
+    @Test
+    void shouldReturnTrueOnFoundNullObject() {
+        MyStack <Object> testObject = new MyStack<>();
+
+        testObject.push(123);
+        testObject.push("Test string first");
+        testObject.push(687);
+        testObject.push(645);
+        testObject.push("Test string second");
+        testObject.push(537);
+        testObject.push(null);
+        testObject.push(348);
+
+        assertTrue(testObject.contains(null));
+    }
+
+    @Test
+    void shouldReturnTrueOnFoundIntegerObject() {
+        MyStack <Object> testObject = new MyStack<>();
+
+        testObject.push(123);
+        testObject.push("Test string first");
+        testObject.push(687);
+        testObject.push(645);
+        testObject.push("Test string second");
+        testObject.push(537);
+        testObject.push(null);
+        testObject.push(348);
+
+        assertTrue(testObject.contains(537));
+    }
+
+    @Test
+    void shouldReturnTrueOnFoundStringObject() {
+        MyStack <Object> testObject = new MyStack<>();
+
+        testObject.push(123);
+        testObject.push("Test string first");
+        testObject.push(687);
+        testObject.push(645);
+        testObject.push("Test string second");
+        testObject.push(537);
+        testObject.push(null);
+        testObject.push(348);
+
+        assertTrue(testObject.contains("Test string first"));
+    }
+
+    @Test
+    void shouldReturnFalseOnUnFoundObject() {
+        MyStack <Object> testObject = new MyStack<>();
+
+        testObject.push(123);
+        testObject.push("Test string first");
+        testObject.push(687);
+        testObject.push(645);
+        testObject.push("Test string second");
+        testObject.push(537);
+        testObject.push(348);
+
+        assertFalse(testObject.contains(0));
+        assertFalse(testObject.contains("Java"));
+        assertFalse(testObject.contains(null));
+    }
+
+    @Test
+    void shouldAddObject() {
+        MyStack <Integer> testObject = new MyStack<>();
+
+        assertTrue(testObject.add(123));
+        assertEquals(123,testObject.peek());
+    }
+
+    @Test
+    void shouldRemoveObject() {
+        MyStack <Object> testObject = new MyStack<>();
+
+        testObject.push(123);
+        testObject.push("Test string first");
+        testObject.push(687);
+        testObject.push(645);
+        testObject.push("Test string second");
+        testObject.push(537);
+        testObject.push(null);
+        testObject.push(348);
+
+        assertTrue(testObject.remove(null));
+        assertTrue(testObject.remove((Object) 537));
+        assertTrue(testObject.remove("Test string second"));
+        assertFalse(testObject.remove((Object) 0));
+        testObject.pop();
+
+        assertEquals(645,testObject.peek());
+    }
+
+    @Test
+    void shouldConvertToArray() {
+        MyStack <Object> testStack = new MyStack<>();
+        Object[] objectsArray = {348,null,537,"Test string second",645,687,"Test string first",123};
+
+        testStack.push(123);
+        testStack.push("Test string first");
+        testStack.push(687);
+        testStack.push(645);
+        testStack.push("Test string second");
+        testStack.push(537);
+        testStack.push(null);
+        testStack.push(348);
+
+        assertArrayEquals(objectsArray,testStack.toArray());
+    }
+
+    @Test
+    void shouldClearMyStack() {
+        MyStack <Object> testStack = new MyStack<>();
+
+        testStack.push(123);
+        testStack.push("Test string first");
+        testStack.push(687);
+        testStack.push(645);
+        testStack.push("Test string second");
+        testStack.push(537);
+        testStack.push(null);
+        testStack.push(348);
+
+        testStack.clear();
+
+        assertEquals(0,testStack.size());
+        Exception exception = assertThrows(RuntimeException.class, testStack::pop);
+        String actualMessage = exception.getMessage();
+        String expectedMessage = "Current MyStack is empty";
+        assertEquals(expectedMessage,actualMessage);
+    }
+
+    @Test
+    void shouldReturnIndexOfElementOrMinus1IfNotFound() {
+        MyStack <Object> testStack = new MyStack<>();
+
+        testStack.push(123);
+        testStack.push("Test string first");
+        testStack.push(687);
+        testStack.push(645);
+        testStack.push("Test string second");
+        testStack.push(537);
+        testStack.push(null);
+        testStack.push(348);
+
+        assertEquals(0,testStack.indexOf((Object) 348));
+        assertEquals(1,testStack.indexOf(null));
+        assertEquals(3,testStack.indexOf("Test string second"));
+        assertEquals(-1,testStack.indexOf((Object) 0));
+    }
+
+    @Test
+    void shouldReturnElementByIndex() {
+        MyStack <Object> testStack = new MyStack<>();
+
+        testStack.push(123);
+        testStack.push("Test string first");
+        testStack.push(687);
+        testStack.push(645);
+        testStack.push("Test string second");
+        testStack.push(537);
+        testStack.push(null);
+        testStack.push(348);
+
+        assertEquals( 348,testStack.get(0));
+        assertEquals(null,testStack.get(1));
+        assertEquals("Test string second",testStack.get(3));
+        assertEquals(123,testStack.get(7));
+
+
+        //Exception test
+        Exception exception = assertThrows(RuntimeException.class,() -> {testStack.get(8);});
+        String actualMessage = exception.getMessage();
+        String expectedMessage = "Index out of range";
+        assertEquals(expectedMessage,actualMessage);
+    }
+
+    @Test
+    void shouldSetElementOnIndex() {
+        MyStack <Object> testStack = new MyStack<>();
+
+        testStack.push(123);
+        testStack.push("Test string first");
+        testStack.push(687);
+        testStack.push(645);
+        testStack.push("Test string second");
+        testStack.push(537);
+        testStack.push(null);
+        testStack.push(348);
+
+        //Exception test
+        Exception exception = assertThrows(RuntimeException.class,() -> {testStack.get(8);});
+        String actualMessage = exception.getMessage();
+        String expectedMessage = "Index out of range";
+        assertEquals(expectedMessage,actualMessage);
+
+
+
+        assertEquals(537,testStack.set(2,204));
+        assertEquals(204,testStack.get(2));
+
+        assertEquals(null,testStack.set(1,"String"));
+        assertEquals("String",testStack.get(1));
+    }
+
+    @Test
+    void shouldAddNodeOnIndexPlaceWithOffsetOfOtherNodes() {
+        MyStack <Object> testStack = new MyStack<>();
+
+        testStack.push(123);
+        testStack.push("Test string first");
+        testStack.push(687);
+        testStack.push(645);
+        testStack.push("Test string second");
+        testStack.push(537);
+        testStack.push(null);
+        testStack.push(348);
+
+        //Exception test
+        Exception exception = assertThrows(RuntimeException.class,() -> {testStack.get(8);});
+        String actualMessage = exception.getMessage();
+        String expectedMessage = "Index out of range";
+        assertEquals(expectedMessage,actualMessage);
+
+
+        testStack.add(2,"Test String");
+        assertEquals("Test String",testStack.get(2));
+        assertEquals(null,testStack.get(1));
+        assertEquals(537,testStack.get(3));
+    }
+
+
+    //Not working correct
+    @Test
+    void shouldRemoveNodeWithOffsetOtherNodes() {
+        MyStack <Object> testStack = new MyStack<>();
+
+        testStack.push(123);
+        testStack.push("Test string first");
+        testStack.push(687);
+        testStack.push(645);
+        testStack.push("Test string second");
+        testStack.push(537);
+        testStack.push(null);
+        testStack.push(348);
+
+        //Exception test
+        Exception exception = assertThrows(RuntimeException.class,() -> {testStack.get(8);});
+        String actualMessage = exception.getMessage();
+        String expectedMessage = "Index out of range";
+        assertEquals(expectedMessage,actualMessage);
+
+        assertEquals(null,testStack.remove(1));
+        testStack.remove(1);
+
+        Object[] arr = testStack.toArray();
+
+        for (Object item : arr) {
+            System.out.println(item);
+        }
+    }
+
+    @Test
+    void testsOfNotImplementedFunctions() {
+        MyStack <Object> testStack = new MyStack<>();
+        List <Object> testList = new LinkedList<>();
+
+        assertNull(testStack.listIterator());
+        assertNull(testStack.listIterator(1));
+        assertNull(testStack.subList(2,3));
+        assertFalse(testStack.retainAll(testList));
+        assertFalse(testStack.removeAll(testList));
+        assertFalse(testStack.containsAll(testList));
+        assertFalse(testStack.addAll(testList));
+        assertFalse(testStack.addAll(2,testList));
+        assertEquals(0,testStack.lastIndexOf((Object) 2));
+    }
+
 }
