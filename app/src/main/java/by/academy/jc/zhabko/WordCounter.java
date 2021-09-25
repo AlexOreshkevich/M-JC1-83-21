6c704f56dbda9879c0846d7f40ae195e99576f9b
Alexey_Zhabko <zhabkoag@gmail.com>
2021-09-25 21:29:42 +0300

    IT'S ALIVE!!!

--- app/src/main/java/by/academy/jc/zhabko/WordCounter.java
+++ app/src/main/java/by/academy/jc/zhabko/WordCounter.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
+import java.util.TreeMap;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -17,12 +18,53 @@ public class WordCounter {
 
     public static void main(String[] args) throws IOException {
         WordCounter test = new WordCounter();
-        //test.countWord();
+        test.countWord();
         System.out.println(test.getRootWord("Caresses"));
         System.out.println(test.getRootWord("ponies"));
         System.out.println(test.getRootWord("ties"));
         System.out.println(test.getRootWord("caress"));
         System.out.println(test.getRootWord("cats"));
+
+        System.out.println(test.getRootWord("Agreed"));
+        System.out.println(test.getRootWord("FEed"));
+
+        System.out.println(test.getRootWord("plastered"));
+        System.out.println(test.getRootWord("bled"));
+
+        System.out.println(test.getRootWord("motoring"));
+        System.out.println(test.getRootWord("sing"));
+
+        System.out.println(test.getRootWord("conflated"));
+        System.out.println(test.getRootWord("troubling"));
+
+        System.out.println(test.getRootWord("hopping"));
+        System.out.println(test.getRootWord("tanned"));
+        System.out.println(test.getRootWord("falling"));
+
+        System.out.println(test.getRootWord("filing"));
+        System.out.println(test.getRootWord("fail"));
+
+        System.out.println(test.getRootWord("happy"));
+        System.out.println(test.getRootWord("sky"));
+
+        System.out.println(test.getRootWord("relational"));
+        System.out.println(test.getRootWord("generalization"));
+        System.out.println(test.getRootWord("sensibiliti"));
+
+        System.out.println(test.getRootWord("triplicate"));
+        System.out.println(test.getRootWord("hopeful"));
+        System.out.println(test.getRootWord("goodness"));
+
+        System.out.println(test.getRootWord("allowance"));
+        System.out.println(test.getRootWord("dependent"));
+        System.out.println(test.getRootWord("effective"));
+
+        System.out.println(test.getRootWord("probate"));
+        System.out.println(test.getRootWord("goodness"));
+
+        System.out.println(test.getRootWord("controll"));
+        System.out.println(test.getRootWord("roll"));
+
 
     }
 
@@ -32,10 +74,10 @@ public class WordCounter {
 
         FileReader input = null;
         StringBuilder allText = new StringBuilder();
-        Map<String,Integer> frequencyMap = new HashMap<>();
+        Map<String,Integer> frequencyMap = new TreeMap<>();
 
         try {
-            input = new FileReader(getResourcePath("xanadu.txt"));
+            input = new FileReader(getResourcePath("Frankenstein.txt"));
 
             int c;
             while ((c = input.read()) != -1) {
@@ -50,7 +92,7 @@ public class WordCounter {
         Matcher matcher = pattern.matcher(allText);
 
         while (matcher.find()) {
-            String word =matcher.group();
+            String word = getRootWord(matcher.group());
             if (!frequencyMap.containsKey(word))  {
                 frequencyMap.put(word,1);
             } else {
@@ -74,6 +116,28 @@ public class WordCounter {
         }
     }
 
+    private boolean containsVowel (CharSequence word) {
+        int i = 0;
+        while (i < word.length()) {
+             if (isVowel(word.charAt(i))) {
+                 return true;
+             }
+             i++;
+        }
+        return false;
+    }
+
+    private boolean isCVC (CharSequence word) {
+        if (word.length() < 3) {
+            return false;
+        }
+        boolean lastCharNotXYW = !(word.charAt(word.length()-1) == 'w' || word.charAt(word.length()-1) == 'x' || word.charAt(word.length()-1) == 'y');
+        boolean c1 = !isVowel(word.charAt(word.length()-3));
+        boolean v = isVowel(word.charAt(word.length()-2));
+        boolean c2 = !isVowel(word.charAt(word.length()-1));
+        return c1 && v && c2 && lastCharNotXYW;
+    }
+
 
     //Porter Stemmer Algorithm
     private String getRootWord(String word) {
@@ -91,19 +155,19 @@ public class WordCounter {
 
         int switcher = 0;
 
-        if (switcher == 0 && (rootWord.subSequence(rootWord.length()-4,rootWord.length()).equals("sses"))) {
+        if (rootWord.length() > 4 && switcher == 0 && (rootWord.subSequence(rootWord.length()-4,rootWord.length()).equals("sses"))) {
             rootWord.delete(rootWord.length()-4,rootWord.length());
             rootWord.append("ss");
             switcher++;
         }
 
-        if (switcher == 0 && (rootWord.subSequence(rootWord.length()-3,rootWord.length()).equals("ies"))) {
+        if (rootWord.length() > 3 && switcher == 0 && (rootWord.subSequence(rootWord.length()-3,rootWord.length()).equals("ies"))) {
             rootWord.delete(rootWord.length()-3,rootWord.length());
             rootWord.append("i");
             switcher++;
         }
 
-        if (switcher == 0 && (rootWord.subSequence(rootWord.length()-2,rootWord.length()).equals("ss"))) {
+        if (rootWord.length() > 2 && switcher == 0 && (rootWord.subSequence(rootWord.length()-2,rootWord.length()).equals("ss"))) {
             rootWord.delete(rootWord.length()-2,rootWord.length());
             rootWord.append("ss");
             switcher++;
@@ -114,7 +178,133 @@ public class WordCounter {
             switcher++;
         }
 
+
+
+        //Step 1b
         switcher = 0;
+
+
+        if (rootWord.length() > 3 && switcher == 0 && ((m>1) && (rootWord.subSequence(rootWord.length()-3,rootWord.length()).equals("eed")))) {
+            rootWord.delete(rootWord.length()-3,rootWord.length());
+            rootWord.append("ee");
+            switcher++;
+        }
+
+        if (rootWord.length() > 2 && switcher == 0 && ((rootWord.subSequence(rootWord.length()-2,rootWord.length()).equals("ed"))&&containsVowel(rootWord.subSequence(0,rootWord.length()-3)))) {
+            rootWord.delete(rootWord.length()-2,rootWord.length());
+            switcher++;
+        }
+
+        if (rootWord.length() > 3 && switcher == 0 && ((rootWord.subSequence(rootWord.length()-3,rootWord.length()).equals("ing"))&&containsVowel(rootWord.subSequence(0,rootWord.length()-4)))) {
+            rootWord.delete(rootWord.length()-3,rootWord.length());
+            switcher++;
+        }
+
+        //Step 1c (cleanup)
+        if (rootWord.length() > 2 && switcher == 1 && rootWord.subSequence(rootWord.length()-2,rootWord.length()).equals("at")) {
+            rootWord.delete(rootWord.length()-2,rootWord.length());
+            rootWord.append("ate");
+            switcher++;
+        }
+
+        if (rootWord.length() > 2 && switcher == 1 && rootWord.subSequence(rootWord.length()-2,rootWord.length()).equals("bl")) {
+            rootWord.delete(rootWord.length()-2,rootWord.length());
+            rootWord.append("ble");
+            switcher++;
+        }
+
+        if (switcher == 1 && !(rootWord.charAt(rootWord.length()-1) == 'l' || rootWord.charAt(rootWord.length()-1) == 's' || rootWord.charAt(rootWord.length()-1) == 'z') && rootWord.charAt(rootWord.length()-1) == rootWord.charAt(rootWord.length()-2)) {
+            rootWord.deleteCharAt(rootWord.length()-1);
+            switcher++;
+        }
+
+        if (switcher == 1 && m >= 1 && isCVC(rootWord)) {
+            rootWord.append("e");
+        }
+
+        switcher = 0;
+
+        //Step 1c (y char)
+        if (rootWord.length() > 1 && switcher == 0 && rootWord.charAt(rootWord.length()-1) == 'y' && containsVowel(rootWord.subSequence(0,rootWord.length()-1))) {
+            rootWord.deleteCharAt(rootWord.length()-1);
+            rootWord.append('i');
+        }
+
+        //Step 2
+        if (rootWord.length() > 7 && switcher == 0 && m > 0 && rootWord.subSequence(rootWord.length()-7,rootWord.length()).equals("ational")) {
+            rootWord.delete(rootWord.length()-7,rootWord.length());
+            rootWord.append("ate");
+            switcher++;
+        }
+
+        if (rootWord.length() > 7 && switcher == 0 && m > 0 && rootWord.subSequence(rootWord.length()-7,rootWord.length()).equals("ization")) {
+            rootWord.delete(rootWord.length()-7,rootWord.length());
+            rootWord.append("ize");
+            switcher++;
+        }
+
+        if (rootWord.length() > 6 && switcher == 0 && m > 0 && rootWord.subSequence(rootWord.length()-6,rootWord.length()).equals("biliti")) {
+            rootWord.delete(rootWord.length()-6,rootWord.length());
+            rootWord.append("ble");
+            switcher++;
+        }
+
+        switcher = 0;
+
+        //Step 3
+        if (rootWord.length() > 5 && switcher == 0 && m > 0 && rootWord.subSequence(rootWord.length()-5,rootWord.length()).equals("icate")) {
+            rootWord.delete(rootWord.length()-5,rootWord.length());
+            rootWord.append("ic");
+            switcher++;
+        }
+
+        if (rootWord.length() > 3 && switcher == 0 && m > 0 && rootWord.subSequence(rootWord.length()-3,rootWord.length()).equals("ful")) {
+            rootWord.delete(rootWord.length()-3,rootWord.length());
+            switcher++;
+        }
+
+        if (rootWord.length() > 4 && switcher == 0 && m > 0 && rootWord.subSequence(rootWord.length()-4,rootWord.length()).equals("ness")) {
+            rootWord.delete(rootWord.length()-4,rootWord.length());
+            switcher++;
+        }
+
+        //Step 4
+        if (rootWord.length() > 4 && switcher == 0 && m > 0 && rootWord.subSequence(rootWord.length()-4,rootWord.length()).equals("ance")) {
+            rootWord.delete(rootWord.length()-4,rootWord.length());
+            switcher++;
+        }
+
+        if (rootWord.length() > 3 && switcher == 0 && m > 0 && rootWord.subSequence(rootWord.length()-3,rootWord.length()).equals("ent")) {
+            rootWord.delete(rootWord.length()-3,rootWord.length());
+            switcher++;
+        }
+
+        if (rootWord.length() > 3 && switcher == 0 && m > 0 && rootWord.subSequence(rootWord.length()-3,rootWord.length()).equals("ive")) {
+            rootWord.delete(rootWord.length()-3,rootWord.length());
+            switcher++;
+        }
+
+        switcher = 0;
+
+        //Step 5 (cleanup)
+
+        if (switcher == 0 && m > 1 && rootWord.charAt(rootWord.length()-1) == 'e') {
+            rootWord.deleteCharAt(rootWord.length()-1);
+            switcher++;
+        }
+
+        if (rootWord.length() > 4 && switcher == 0 && m >= 1 && !isCVC(rootWord) && rootWord.subSequence(rootWord.length()-4,rootWord.length()).equals("ness")) {
+            rootWord.delete(rootWord.length()-4,rootWord.length());
+        }
+
+        //Step 5b
+        switcher = 0;
+
+        if (rootWord.length() > 2 && m > 1 && rootWord.charAt(rootWord.length()-1) == 'l' && rootWord.charAt(rootWord.length()-2) == 'l') {
+            rootWord.deleteCharAt(rootWord.length()-1);
+        }
+
+
 
 
 
