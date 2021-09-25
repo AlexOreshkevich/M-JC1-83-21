4701fe2bcda1308e8c9603eb894ddc149c02aec1
Alexey_Zhabko <zhabkoag@gmail.com>
2021-09-25 19:00:27 +0300

    Step 1a Porter Stemmer Algorithm

--- app/src/main/java/by/academy/jc/zhabko/WordCounter.java
+++ app/src/main/java/by/academy/jc/zhabko/WordCounter.java
@@ -5,6 +5,7 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -16,7 +17,13 @@ public class WordCounter {
 
     public static void main(String[] args) throws IOException {
         WordCounter test = new WordCounter();
-        test.countWord();
+        //test.countWord();
+        System.out.println(test.getRootWord("Caresses"));
+        System.out.println(test.getRootWord("ponies"));
+        System.out.println(test.getRootWord("ties"));
+        System.out.println(test.getRootWord("caress"));
+        System.out.println(test.getRootWord("cats"));
+
     }
 
 
@@ -43,7 +50,7 @@ public class WordCounter {
         Matcher matcher = pattern.matcher(allText);
 
         while (matcher.find()) {
-            String word = matcher.group();
+            String word =matcher.group();
             if (!frequencyMap.containsKey(word))  {
                 frequencyMap.put(word,1);
             } else {
@@ -52,5 +59,68 @@ public class WordCounter {
             }
         }
         System.out.println(frequencyMap);
+    }
+
+    private boolean isVowel (char ch) {
+        switch (ch) {
+            case 'a':
+            case 'e':
+            case 'i':
+            case 'o':
+            case 'u':
+                return true;
+            default:
+                return false;
+        }
+    }
+
+
+    //Porter Stemmer Algorithm
+    private String getRootWord(String word) {
+        //Step 0
+        StringBuilder rootWord = new StringBuilder(word.toLowerCase(Locale.ROOT));
+        int m = 0;
+        for (int i=0; i<rootWord.length()-1; i++) {
+            if (isVowel(rootWord.charAt(i))&&!isVowel(rootWord.charAt(i+1))) {
+                m++;
+            }
+        }
+
+
+        //Step 1a
+
+        int switcher = 0;
+
+        if (switcher == 0 && (rootWord.subSequence(rootWord.length()-4,rootWord.length()).equals("sses"))) {
+            rootWord.delete(rootWord.length()-4,rootWord.length());
+            rootWord.append("ss");
+            switcher++;
+        }
+
+        if (switcher == 0 && (rootWord.subSequence(rootWord.length()-3,rootWord.length()).equals("ies"))) {
+            rootWord.delete(rootWord.length()-3,rootWord.length());
+            rootWord.append("i");
+            switcher++;
+        }
+
+        if (switcher == 0 && (rootWord.subSequence(rootWord.length()-2,rootWord.length()).equals("ss"))) {
+            rootWord.delete(rootWord.length()-2,rootWord.length());
+            rootWord.append("ss");
+            switcher++;
+        }
+
+        if (switcher == 0 && (rootWord.charAt(rootWord.length()-1)=='s')) {
+            rootWord.deleteCharAt(rootWord.length()-1);
+            switcher++;
+        }
+
+        switcher = 0;
+
+
+
+
+
+
+        return rootWord.toString();
     }
 }
