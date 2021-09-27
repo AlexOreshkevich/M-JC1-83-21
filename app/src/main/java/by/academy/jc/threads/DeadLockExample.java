9d0cfdf782acdf3116132fd84ab90238a85cffeb
Aliaksandr Arashkevich <alexander.oreshkevich@gmail.com>
2021-09-27 19:53:29 +0300

    Deadlock example

--- app/src/main/java/by/academy/jc/threads/DeadLockExample.java
+++ app/src/main/java/by/academy/jc/threads/DeadLockExample.java
@@ -4,18 +4,31 @@ public class DeadLockExample {
 
   public void method1() {
     synchronized (String.class) {
-      System.out.println("Acquired lock on String.class object");
+
+      System.out.printf(
+          "[%s] Acquired lock on String.class object \n",
+          Thread.currentThread().getName()
+      );
       synchronized (Integer.class) {
-        System.out.println("Acquired lock on Integer.class object");
+        System.out.printf(
+            "[%s] Acquired lock on Integer.class object \n",
+            Thread.currentThread().getName()
+        );
       }
     }
   }
 
   public void method2() {
     synchronized (Integer.class) {
-      System.out.println("Acquired lock on Integer.class object");
+      System.out.printf(
+          "[%s] Acquired lock on Integer.class object \n",
+          Thread.currentThread().getName()
+      );
       synchronized (String.class) {
-        System.out.println("Acquired lock on String.class object");
+        System.out.printf(
+            "[%s] Acquired lock on String.class object \n",
+            Thread.currentThread().getName()
+        );
       }
     }
   }
@@ -27,8 +40,8 @@ public class DeadLockExample {
     Runnable t1 = example::method1;
     Runnable t2 = example::method2;
 
-    Thread thread1 = new Thread(t1);
-    Thread thread2 = new Thread(t2);
+    Thread thread1 = new Thread(t1, "first");
+    Thread thread2 = new Thread(t2, "second");
 
     thread1.start();
     thread2.start();
